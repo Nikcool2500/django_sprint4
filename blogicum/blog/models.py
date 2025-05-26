c6331db9ca15eeb962f4abf56429fd41cfa8b580
Nikita Chirka <73420189+Nikcool2500@users.noreply.github.com>
2025-05-26 15:00:06 +0300

    Сделано без критериев ксеныча

---
+++ blogicum/blog/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.db.models import Count
+
+
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    is_published = models.BooleanField(
+        "Опубликовано",
+        default=True,
+        help_text="Снимите галочку, чтобы скрыть публикацию.",
+    )
+    created_at = models.DateTimeField("Добавлено", auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Location(BaseModel):
+    name = models.CharField("Название места", max_length=256)
+
+    class Meta:
+        verbose_name = "местоположение"
+        verbose_name_plural = "Местоположения"
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BaseModel):
+    title = models.CharField("Заголовок", max_length=256)
+    description = models.TextField("Описание")
+    slug = models.SlugField(
+        "Идентификатор",
+        unique=True,
+        help_text="Идентификатор страницы для URL; разрешены символы латиницы,"
+        " цифры, дефис и подчёркивание.",
+    )
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.title
+
+
+class PostQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(
+            is_published=True, category__is_published=True, pub_date__lte=now
+        ).annotate(comment_count=Count("comments"))
+
+
+class Post(BaseModel):
+    title = models.CharField("Заголовок", max_length=256)
+    text = models.TextField("Текст")
+    pub_date = models.DateTimeField(
+        "Дата и время публикации",
+        help_text="Если установить дату и время в будущем — можно делать "
+        "отложенные публикации.",
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Автор публикации"
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Местоположение",
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, verbose_name="Категория"
+    )
+    image = models.ImageField(
+        upload_to="posts_images",
+        blank=True,
+        verbose_name="Изображение",
+    )
+
+    objects = PostQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = "публикация"
+        verbose_name_plural = "Публикации"
+        ordering = ["-pub_date"]
+        default_related_name = "posts"
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name="Текст комментария")
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("created_at",)
